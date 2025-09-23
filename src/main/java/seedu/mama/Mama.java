62df26251cabc477945e2abee3666e16bbb8cae1
yujie-o <yujie2164@gmail.com>
2025-09-24 00:51:09 +0800

    refactor: rename Duke to Mama across codebase

---
+++ src/main/java/seedu/mama/Mama.java
@@ -0,0 +1,13 @@
+package seedu.mama;
+
+/**
+ * Main entry-point for the MAMA application.
+ */
+public class Mama {
+    /**
+     * Starts the CLI loop.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
